169bd749cfbb36f87a4a474d4e9f827b8045c07c
Karim-Chi <2010216@brunel.ac.uk>
2022-07-02 13:55:44 +0100

    Today created all oages and navbar also added home css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import React from 'react'
 import './App.css';
-
+import Navbar from "./component/Navbar";
+import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-     <>SUIIIIIIIIIIIIIIIIIIIIIIIIIIIII</>
+    <Router>
+        <Navbar/>
+        <Routes>
+        <Route exact path='/' element={<Home />} />  
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import Logo from '../assets/slazlogo.png'
+import Logo from "../assets/slazlogo.PNG"
+import { Link } from 'react-router-dom'
+import "../styles/Navbar.css";
+
 function Navbar() {
   return (
     <div className='navbar'> 
     <div className='leftSide'>
-        <img src={Logo}/>
+        <img src={Logo} alt="logo is about slazyo's" />
     </div>
-    <div className='rightSide'></div>
+    <div className='rightSide'>
+      <Link to="/"> Home </Link>
+      <Link to="/menu"> Menu </Link>
+      <Link to="/about"> About </Link>
+      <Link to="/contact"> Contact </Link>
+    </div>
     </div>
   )
 }
